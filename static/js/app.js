4aefedc72aae88ba46f2969c73621af6138130f3
Lori B <47092293+loribeth18@users.noreply.github.com>
2019-05-20 18:17:16 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -9,13 +9,11 @@ console.log(tableData);
 tableData.forEach(function(ufodata) {
     console.log(ufodata);
     //var row = tbody.append("tr");
-    for (var i = 0; i < 12; i++) {
-        trow = tbody.append("tr");
-        Object.entries(ufodata).forEach(function([key,value]) {
-        var cell = trow.append("td")
-        cell.text(value);
+    trow = tbody.append("tr");
+    Object.entries(ufodata).forEach(function([key,value]) {
+    var cell = trow.append("td")
+    cell.text(value);
         })
-    };
 });
 
 
@@ -23,27 +21,24 @@ tableData.forEach(function(ufodata) {
 var filterbutton = d3.select("#filter-btn")
 
 filterbutton.on("click",function(){
-    console.log("clicked")
-    d3.event.preventDefault();
-    var inputText = d3.event.target.value;
-});
-
-
-var datetofilter = d3.select("#datetime")
-
-datetofilter.on("click", function() {
-    console.log("clicked")
+    console.log("clicked");
     d3.event.preventDefault();
 
     var datetinput = d3.select("#datetime")
     var datefilter = datetinput.node().value;
-    
-    ufofilter =datefilter.filter(ufodata)
-        console.log(ufodata);
-        Object.entries(datefilter).forEach(function([key,value]) {
-            trow = tbody.append("tr");
-            var cell = row.append("td")
-            cell.text(value);
-        }); 
-});
 
+    var ufofilterdate = tableData.filter(row => row.datetime === datefilter);//.filter(row => row.city==="cedar rapids");
+
+    tbody.html("");
+    
+    //Step 1: Loop Through `data` and console.log each report object
+    ufofilterdate.forEach(function(ufodata) {
+        console.log(ufodata);
+        //var row = tbody.append("tr");
+        trow = tbody.append("tr");
+        Object.entries(ufodata).forEach(function([key,value]) {
+        var cell = trow.append("td")
+        cell.text(value);
+        });
+    });
+});
